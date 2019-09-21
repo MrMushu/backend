3646a754e95c9f8343c686fdb6786ff88a129765
MrMushu <aaokura@gmail.com>
2019-09-20 19:26:08 -0700

    'new'

--- backend/settings.py
+++ backend/settings.py
@@ -81,9 +81,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ORIGIN_WHITELIST = (
-    'localhost:3000'
-)
+# CORS_ORIGIN_WHITELIST = (
+#    'localhost:3000'
+# )
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'backend.urls'
 
